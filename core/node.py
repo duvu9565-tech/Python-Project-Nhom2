7da658bb59ebdef262fc388bcd463e610d485448
dinhhung2040-design <dinhhung2040@gmail.com>
2026-06-17 21:53:29 +0700

    Update node.py

--- core/node.py
+++ core/node.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, data=Non):
         self.data = data       # Chứa đối tượng dữ liệu hàng hóa
         self.next = None       # Con trỏ trỏ đến Node tiếp theo
         self.prev = None       # Con trỏ trỏ đến Node phía trước
