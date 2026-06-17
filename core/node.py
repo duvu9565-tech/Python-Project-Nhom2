12884cf6cb1b3ca5d0c1ffe120d8f4c84a0aa31e
dinhhung2040-design <dinhhung2040@gmail.com>
2026-06-17 18:41:34 +0700

    Create Node class for linked list implementation
    Define Node class with data and pointers for linked list.

---
+++ core/node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data       # Chứa đối tượng dữ liệu hàng hóa
+        self.next = None       # Con trỏ trỏ đến Node tiếp theo
+        self.prev = None       # Con trỏ trỏ đến Node phía trước
